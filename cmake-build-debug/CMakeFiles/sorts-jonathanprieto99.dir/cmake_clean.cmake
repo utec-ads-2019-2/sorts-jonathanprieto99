d5b0149914fc4c1c21e58e1b64af40160613bf8f
Jonathan Prieto <jonathan.prieto@me.com>
2019-08-28 19:38:00 -0500

    First Upload

---
+++ cmake-build-debug/CMakeFiles/sorts-jonathanprieto99.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sorts-jonathanprieto99.dir/main.cpp.o"
+  "sorts-jonathanprieto99.pdb"
+  "sorts-jonathanprieto99"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sorts-jonathanprieto99.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
